ad35ef6561a0b2e666d6c380c8560e648298284a
i447092 <i447092@fhict.nl>
2020-12-30 23:23:54 +0100

    Created basic model classes

---
+++ Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backend_school_api.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public string Total { get; set; }
+        public string Adres { get; set; }
+        public string HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+        public DateTime OrderDate { get; set; }
+        public virtual List<OrderLine> OrderProducts { get; set; }
+    }
+}
---
+++ Models/OrderLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace backend_school_api.Models
+{
+    public class OrderLine
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Amount { get; set; }
+    }
+}
